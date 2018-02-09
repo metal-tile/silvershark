99f114609221bb0a0945011099c94da2c4b35d6b
sinmetal <metal.tie@gmail.com>
2018-02-09 10:08:15 +0900

    とりあえずpub/subにpublishできた
    messageの内容にeventをまるっと入れたが、結構なサイズになっているので、必要なものだけを入れた方が良さそう

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,10 @@
+'use strict';
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const PubSub = require('@google-cloud/pubsub');
+
+const pubsub = PubSub();
 
 admin.initializeApp(functions.config().firebase);
 
@@ -11,4 +16,19 @@ exports.onPlayerPositionChanged = functions.firestore
         var document = event.data.data();
         console.log(document);
         console.log(event);
+
+        const topic = pubsub.topic("projects/metal-tile-dev1/topics/player-position");
+        console.log(topic);
+
+        const  data = {
+            message: event
+        };
+        const publisher = topic.publisher();
+        return publisher.publish(Buffer.from(JSON.stringify(data)))
+            .then(() => res.status(200).send('Message published.'))
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send(err);
+                return Promise.reject(err);
+            });
     });
